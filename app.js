b38a5e2c8c5673c5ec839dae7392cfde6b12d615
iHeavenszi <iheavenszi75@gmail.com>
2024-12-09 03:21:39 -0600

    server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set("port", 4000);
 
 // Configura CORS antes de las rutas
 app.use(cors({
-  origin: 'http://darkysfishshop.com.mx/', // Asegúrate de usar tu dominio aquí
+  origin: '192.185.131.38/', // Asegúrate de usar tu dominio aquí
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
 }));
