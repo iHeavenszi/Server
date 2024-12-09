903de75d3432202e950642bd0b6152b21ffc596a
iHeavenszi <iheavenszi75@gmail.com>
2024-12-09 04:25:46 -0600

    final

--- schema/user.js
+++ schema/user.js
@@ -21,6 +21,10 @@ const User = sequelize.define("User", {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -24,6 +24,7 @@ router.post("/",async (req, res)=>{
                     id: newUser.id,
                     username: newUser.username,
                     name: newUser.name,
+                    email: newUser.email,
                 },
             })
         );
